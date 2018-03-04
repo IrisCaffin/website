9c01e3a1351f86f6dd75a86011491b69ab7f74cb
IrisCaffin <iriscaffin@gmail.com>
2018-03-03 18:29:20 -0800

    Update LinkedIn and GitHub text

--- website/src/components/home/Home.js
+++ website/src/components/home/Home.js
@@ -17,9 +17,9 @@ class Home extends Component {
                <h4 className="purple">I'm Iris Caffin</h4>
                <p>I am a Web Developer specializing in front-end development with a background in television production. Versed in numerous programming languages including ReactJS, JavaScript, HTML/CSS, and Sass.</p><br/>
                <h6 className="purple">LinkedIn</h6>
-               <p>Check out my LinkedIn page <a href="https://www.linkedin.com/in/iriscaffin/" target="_blank" rel="noopener noreferrer">here</a>.</p>
+               <p>Reach out or check out my LinkedIn page <a href="https://www.linkedin.com/in/iriscaffin/" target="_blank" rel="noopener noreferrer">here</a>.</p>
                  <h6 className="purple">GitHub</h6>
-                 <p>Check out my GitHub <a href="https://github.com/IrisCaffin" target="_blank" rel="noopener noreferrer">here</a>.</p>
+                 <p>Check out my projects on GitHub <a href="https://github.com/IrisCaffin" target="_blank" rel="noopener noreferrer">here</a>.</p>
              </div>
              <div className="col-md-5 pull-md-7">
                <img className="iris" alt=""/>
